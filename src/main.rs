f6a89cd55e5bbb933db28000f5b6810ab48ac722
Yifeng Liu <yifeliu@microsoft.com>
2021-08-13 17:22:04 +0800

    rework map

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use quicksilver::{
     Future
 };
 
+use rand::Rng;
 use std::collections::HashMap;
 
 
@@ -16,11 +17,26 @@ struct Game {
     mononoki_font_info: Asset<Image>,
     map_size: Vector,
     map: Vec<Tile>,
-    entities: Vec<Entity>,
-    player_id: usize,
     tileset: Asset<HashMap<char, Image>>,
     tile_size_px: Vector,
+    cursor: Cursor,
+    selected_tile: SelectedTile,
 }
+
+
+#[derive(Clone, Debug, PartialEq)]
+struct Cursor {
+    pos: Vector,
+    glyph: char,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct SelectedTile {
+    pos: Vector,
+    glyph: char,
+    is_selected: bool,
+}
+
 
 #[derive(Clone, Debug, PartialEq)]
 struct Tile {
@@ -29,26 +45,51 @@ struct Tile {
     color: Color,
 }
 
+fn gen_random_tile_color() -> Color {
+    let mut rng = rand::thread_rng();
+    match rng.gen_range(0..4) {
+        0 => Color::CYAN,
+        1 => Color::ORANGE,
+        2 => Color::MAGENTA,
+        3 => Color::PURPLE,
+        _ => Color::WHITE,
+    }
+}
+
+
 fn generate_map(size: Vector) -> Vec<Tile> {
     let width = size.x as usize;
     let height = size.y as usize;
     let mut map = Vec::with_capacity(width * height);
     for x in 0..width {
         for y in 0..height {
-            let mut tile = Tile {
+            let glyph = if x == 0 || x == width - 1 || y == 0 || y == height - 1 {
+                '#'
+            } else {
+                'o'
+            };
+            let color = if x == 0 || x == width - 1 || y == 0 || y == height - 1 {
+                Color::BLACK
+            } else {
+                gen_random_tile_color()
+            };
+            let tile = Tile {
                 pos: Vector::new(x as f32, y as f32),
-                glyph: '.',
-                color: Color::BLACK,
+                glyph: glyph,
+                color: color,
             };
 
-            if x == 0 || x == width - 1 || y == 0 || y == height - 1 {
-                tile.glyph = '#';
-            };
             map.push(tile);
         }
     }
     map
 }
+
+fn is_adjacent_tiles(x: f32, y: f32, xx: f32, yy: f32) -> bool {
+    // Diagnals not included.
+    return (x - xx).abs() + (y - yy).abs() <= 1.0;
+}
+
 
 #[derive(Clone, Debug, PartialEq)]
 struct Entity {
@@ -59,38 +100,6 @@ struct Entity {
     max_hp: i32,
 }
 
-fn generate_entities() -> Vec<Entity> {
-    vec![
-        Entity {
-            pos: Vector::new(9, 6),
-            glyph: 'g',
-            color: Color::RED,
-            hp: 1,
-            max_hp: 1,
-        },
-        Entity {
-            pos: Vector::new(2, 4),
-            glyph: 'g',
-            color: Color::RED,
-            hp: 1,
-            max_hp: 1,
-        },
-        Entity {
-            pos: Vector::new(7, 5),
-            glyph: '%',
-            color: Color::PURPLE,
-            hp: 0,
-            max_hp: 0,
-        },
-        Entity {
-            pos: Vector::new(4, 8),
-            glyph: '%',
-            color: Color::PURPLE,
-            hp: 0,
-            max_hp: 0,
-        },
-    ]
-}
 
 impl State for Game {
     // Load the assets and initialise the game
@@ -110,19 +119,9 @@ impl State for Game {
 
         let map_size = Vector::new(20, 15);
         let map = generate_map(map_size);
-        let mut entities = generate_entities();
-
-        let player_id = entities.len();
-        entities.push(Entity {
-            pos: Vector::new(5, 3),
-            glyph: '@',
-            color: Color::BLUE,
-            hp: 3,
-            max_hp: 5,
-        });
 
         let tile_size_px = Vector::new(24, 24);
-        let game_glyphs = "#@g.%";
+        let game_glyphs = "#o_S";
 
         let tileset = Asset::new(Font::load(font_sqaure).and_then(move |text| {
             let tiles = text
@@ -137,15 +136,26 @@ impl State for Game {
             Ok(tileset)
         }));
 
+        let cursor = Cursor {
+            pos: Vector::new(1, 1),
+            glyph: '_',
+        };
+
+        let selected_tile = SelectedTile {
+            pos: Vector::new(0, 0),
+            glyph: 'S',
+            is_selected: false,
+        };
+
         Ok(Self {
             title,
             mononoki_font_info,
             map_size,
             map,
-            entities,
-            player_id,
             tileset,
             tile_size_px,
+            cursor,
+            selected_tile,
         })
     }
 
@@ -153,18 +163,70 @@ impl State for Game {
     fn update(&mut self, window: &mut Window) -> Result<()> {
         use ButtonState::*;
 
-        let player = &mut self.entities[self.player_id];
-        if window.keyboard()[Key::Left] == Pressed {
-            player.pos.x -= 1.0;
+        let (cursor, selected_tile, map_size) = (&mut self.cursor, &mut self.selected_tile, &self.map_size);
+        let x = cursor.pos.x as usize;
+        let y = cursor.pos.y as usize;
+
+        // Move left inbound.
+        if (window.keyboard()[Key::Left] == Pressed || window.keyboard()[Key::H] == Pressed)
+            && x > 1 {
+                if !selected_tile.is_selected {
+                    cursor.pos.x -= 1.0;
+                } else if is_adjacent_tiles(cursor.pos.x - 1.0, cursor.pos.y, selected_tile.pos.x, selected_tile.pos.y) {
+                    cursor.pos.x -= 1.0;
+                } else {
+                    // Do nothing.
+                }
+            }
+        // Move right inbound.
+        if (window.keyboard()[Key::Right] == Pressed || window.keyboard()[Key::L] == Pressed)
+            && map_size.x - 2.0 > cursor.pos.x {
+                if !selected_tile.is_selected {
+                    cursor.pos.x += 1.0;
+                } else if is_adjacent_tiles(cursor.pos.x + 1.0, cursor.pos.y, selected_tile.pos.x, selected_tile.pos.y) {
+                    cursor.pos.x += 1.0;
+                } else {
+                    // Do nothing.
+                }
+            }
+        // Move up inbound.
+        if (window.keyboard()[Key::Up] == Pressed || window.keyboard()[Key::K] == Pressed )
+            && y > 1 {
+                if !selected_tile.is_selected {
+                    cursor.pos.y -= 1.0;
+                } else if is_adjacent_tiles(cursor.pos.x, cursor.pos.y - 1.0, selected_tile.pos.x, selected_tile.pos.y) {
+                    cursor.pos.y -= 1.0;
+                } else {
+                    // Do nothing.
+                }
+            }
+        // Move down inbound.
+        if (window.keyboard()[Key::Down] == Pressed || window.keyboard()[Key::J] == Pressed)
+            && map_size.y - 2.0 > cursor.pos.y {
+                if !selected_tile.is_selected {
+                    cursor.pos.y += 1.0;
+                } else if is_adjacent_tiles(cursor.pos.x, cursor.pos.y + 1.0, selected_tile.pos.x, selected_tile.pos.y) {
+                    cursor.pos.y += 1.0;
+                } else {
+                    // Do nothing.
+                }
+            }
+        // Unselecte tile.
+        if window.keyboard()[Key::Space] == Pressed {
+            selected_tile.is_selected = false;
         }
-        if window.keyboard()[Key::Right] == Pressed {
-            player.pos.x += 1.0;
-        }
-        if window.keyboard()[Key::Up] == Pressed {
-            player.pos.y -= 1.0;
-        }
-        if window.keyboard()[Key::Down] == Pressed {
-            player.pos.y += 1.0;
+        // Selete tile or unselete a selected one.
+        if window.keyboard()[Key::Return] == Pressed {
+            // println!("<{}, {}>", cursor.pos.x, cursor.pos.y);
+            if selected_tile.is_selected && selected_tile.pos.x == cursor.pos.x && selected_tile.pos.y == cursor.pos.y {
+                // Unselect a selected tile.
+                selected_tile.is_selected = false;
+            } else {
+                // Select a tile.
+                selected_tile.pos.x = cursor.pos.x;
+                selected_tile.pos.y = cursor.pos.y;
+                selected_tile.is_selected = true;
+            }
         }
 
         Ok(())
@@ -198,11 +260,20 @@ impl State for Game {
         let offset_px = Vector::new(50, 120);
 
         // Draw map.
-        let (tileset, map) = (&mut self.tileset, &self.map);
+        let (tileset, map, selected) = (&mut self.tileset, &self.map, &self.selected_tile);
         tileset.execute(|tileset| {
             for tile in map.iter() {
-                if let Some(image) = tileset.get(&tile.glyph) {
-                    let pos_px = tile.pos.times(tile_size_px);
+                let pos_px = tile.pos.times(tile_size_px);
+                if selected.is_selected && selected.pos.x == tile.pos.x && selected.pos.y == tile.pos.y {
+                    // Draw selected tile, if any.
+                    if let Some(image) = tileset.get(&'0') {
+                        window.draw(
+                            &Rectangle::new(offset_px + pos_px, image.area().size()),
+                            Blended(&image, tile.color),
+                            );
+                    }
+                } else if let Some(image) = tileset.get(&tile.glyph) {
+                    // Draw tile.
                     window.draw(
                         &Rectangle::new(offset_px + pos_px, image.area().size()),
                         Blended(&image, tile.color),
@@ -212,17 +283,15 @@ impl State for Game {
             Ok(())
         })?;
 
-        // Draw entities.
-        let (tileset, entities) = (&mut self.tileset, &self.entities);
+        // Draw cursor.
+        let (tileset, cursor) = (&mut self.tileset, &self.cursor);
         tileset.execute(|tileset| {
-            for entity in entities.iter() {
-                if let Some(image) = tileset.get(&entity.glyph) {
-                    let pos_px = offset_px + entity.pos.times(tile_size_px);
-                    window.draw(
-                        &Rectangle::new(pos_px, image.area().size()),
-                        Blended(&image, entity.color),
-                        );
-                }
+            if let Some(image) = tileset.get(&cursor.glyph) {
+                let pos_px = offset_px + cursor.pos.times(tile_size_px);
+                window.draw(
+                    &Rectangle::new(pos_px, image.area().size()),
+                    Blended(&image, Color::BLACK),
+                    );
             }
             Ok(())
         })?;
@@ -230,6 +299,7 @@ impl State for Game {
         Ok(())
     }
 }
+
 
 fn main() {
     // let settings = Settings {
